b4bf529308511b32540e08f1f5ecc70a06ca3b8c
JaewonLee11 <ddugi1102@gmail.com>
2024-10-10 08:23:56 +0900

    Post model 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField()
+
 # Create your models here.
