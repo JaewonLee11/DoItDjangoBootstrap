b2c34eb4963a416451c1c47d4f8d3f4291c2bfa1
JaewonLee11 <ddugi1102@gmail.com>
2024-10-10 22:53:37 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/index.html'
 
 #
 # def index(request):
@@ -18,18 +14,35 @@ class PostListView(ListView):
 #             'post_list' : posts
 #         }
 #     )
+
+# ---->
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    # 혹은 ListlView 클래스는 _list.html를 자동으로 인식하므로 index.html의 이름을 post_list.html로 변경
+
+
+
 #
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post' : post,
+#         }
+#
+#     )
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
+# ---->
 
-    )
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/single_post_page.html'
+    # 혹은 DetailView 클래스는 _detail.html를 자동으로 인식하므로 single_post_page.html의 이름을 post_detail.html로 변경
 
 
 # Create your views here.
