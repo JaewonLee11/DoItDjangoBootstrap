446ad9342d2f1268ed6508d709cd1439dd734d60
JaewonLee11 <ddugi1102@gmail.com>
2024-10-19 23:45:27 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from django.views.generic import ListView, DetailView
 #
 #     return render(
 #         request,
-#         'blog/index.html',
+#         'blog/post_list.html',
 #         {
 #             'post_list' : posts
 #         }
@@ -19,8 +19,7 @@ from django.views.generic import ListView, DetailView
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/index.html'
-    # 혹은 ListlView 클래스는 _list.html를 자동으로 인식하므로 index.html의 이름을 post_list.html로 변경
+
 
 
 
@@ -30,7 +29,7 @@ class PostListView(ListView):
 #
 #     return render(
 #         request,
-#         'blog/single_post_page.html',
+#         'blog/post_detail.html',
 #         {
 #             'post' : post,
 #         }
@@ -41,8 +40,7 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/single_post_page.html'
-    # 혹은 DetailView 클래스는 _detail.html를 자동으로 인식하므로 single_post_page.html의 이름을 post_detail.html로 변경
+
 
 
 # Create your views here.
