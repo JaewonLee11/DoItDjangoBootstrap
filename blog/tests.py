d72bd433977a844c162ee7ae44d3a496317a9d1b
JaewonLee11 <ddugi1102@gmail.com>
2024-10-21 10:41:41 +0900

    포스트 목록, 상세페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 from django.test import TestCase, Client
 from .models import Post
-
+from django.contrib.auth.models import User
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', password='trum1234')
+        self.user_obama = User.objects.create_user(username='obama', password='obam1234')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -45,10 +47,12 @@ class TestView(TestCase):
         post_001=Post.objects.create(
             title='첫 번쨰 포스트입니다.',
             content='Hello World!',
+            author=self.user_trump,
         )
         post_002=Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
+            author=self.user_obama,
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -64,6 +68,9 @@ class TestView(TestCase):
 
         # '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
 
     def test_post_detail(self):
         # 1.1. 포스트가 하나 있다.
