02ef765bf131d69a570b22e1f0b01eb61fc7eb61
JaewonLee11 <ddugi1102@gmail.com>
2024-10-22 11:57:29 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
@@ -10,6 +10,7 @@ class PostList(ListView):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['tags'] = Tag.objects.all()
         return context
 def category_page(request, slug):
     if slug == 'no_category':
@@ -29,6 +30,23 @@ def category_page(request, slug):
             'category' : category,
         }
     )
+
+def tag_page(request,slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+
+        }
+    )
+
 
 class PostDetail(DetailView):
     model = Post
