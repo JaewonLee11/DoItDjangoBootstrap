ff04c6360ce9628d6a5528407e40ed4ad34f16b0
JaewonLee11 <ddugi1102@gmail.com>
2024-10-23 13:01:19 +0900

    구글 로그인 기능 추가하기

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Post, Category, Tag
 from markdownx.admin import MarkdownxModelAdmin
 
-admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -46,6 +46,13 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'markdownx',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'do_it_django_prj.urls'
@@ -137,3 +145,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
